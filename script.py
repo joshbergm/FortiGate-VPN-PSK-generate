518d6b181647ee00b2c84da1235e2ea47c174cd1
Joshua Bergman <josh.bergman@outlook.com>
2022-07-16 15:15:37 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,46 @@
+from netmiko import ConnectHandler
+import random
+import string
+import time
+
+#Specify connection details
+remote_fortigate = { 'host': '<remote_hostname>',
+      'device_type': 'fortinet',
+      'ip': '<IP>',
+      'username': '<user>',
+      'password': '<password>' }
+
+local_fortigate = { 'host': '<local_hostname>',
+      'device_type': 'fortinet',
+      'ip': '<ip>',
+      'username': '<username>',
+      'password': '<password>' }
+
+#Generate PSK
+def generate_key(length):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in
+    range(length))
+
+VPN_PSK = generate_key(16)
+
+#Generate command set
+REMOTE_COMMANDS = ['config vpn ipsec phase1-interface',
+                      'edit "<VPN NAME>"',
+                      'set psk ' + VPN_PSK]
+
+LOCAL_COMMANDS = ['config vpn ipsec phase1-interface',
+                     'edit "<VPN NAME>"',
+                     'set psk ' + VPN_PSK]
+
+#Start SSH connection (remote, local)
+SSH_REMOTE_FORTIGATE = ConnectHandler(**remote_fortigate)
+SSH_LOCAL_FORTIGATE = ConnectHandler(**local_fortigate)
+
+#Send SSH commands (remote, local)
+SSH_REMOTE_FORTIGATE.send_config_set(HICT_VPN_COMMANDS)
+time.sleep(5)
+SSH_LOCAL_FORTIGATE.send_config_set(BICT_VPN_COMMANDS)
+
+#Stop SSH connection (remote, local)
+SSH_REMOTE_FORTIGATE.disconnect()
+SSH_LOCAL_FORTIGATE.disconnect()
